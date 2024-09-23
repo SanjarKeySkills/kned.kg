1629ae772709716ed4a8a938eb68495a2a7d8b18
SanjarKeySkills <forest2004@mail.ru>
2024-09-23 16:36:09 +0600

    230924 | heroHeder complete

--- src/components/Widget/LayoutsComponentsBlock/HeroHeader/heroHeader.jsx
+++ src/components/Widget/LayoutsComponentsBlock/HeroHeader/heroHeader.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import styles from "./index.module.scss";
+import styles from "./heroHeader.module.scss";
 
-const Hero = () => {
+const HeroHeader = () => {
     return (
-        <div className={styles.container}>
+        <div className={styles.heroHeader}>
             <div className={styles.heroTop}>
                 <h1 className={styles.heroText}>
                     Палата Независимых Директоров
                 </h1>
                 <h1 className={styles.heroText}>Кыргызской Республики</h1>
             </div>
-            <div className={styles.heroBottom}></div>
         </div>
     );
 };
 
-export default Hero;
+export default HeroHeader;
