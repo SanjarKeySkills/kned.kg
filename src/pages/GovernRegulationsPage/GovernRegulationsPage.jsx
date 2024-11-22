e4fd48bc6dc4d346a14fc34d47aa627f68615733
SanjarKeySkills <forest2004@mail.ru>
2024-11-22 17:20:19 +0600

    22/11/24 | Criteria changes, pages govern docs

---
+++ src/pages/GovernRegulationsPage/GovernRegulationsPage.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./governRegulationsPage.module.scss";
+
+const GovernRegulationsPage = () => {
+    return (
+        <>
+            <div className={styles.charterPage}>
+                <div className={styles.charterPageWrapper}>
+                    <span></span>
+                    <h1>ПОЛОЖЕНИЕ ОБ ОРГАНАХ УПРАВЛЕНИЯ</h1>
+                    <h2>
+                        ОБЩЕСТВЕННОГО ОБЪЕДИНЕНИЯ <br /> «ПАЛАТА НЕЗАВИСИМЫХ
+                        ДИРЕКТОРОВ <br /> КЫРГЫЗСКОЙ РЕСПУБЛИКИ»
+                    </h2>
+                    <p>г. БИШКЕК – 2024 год</p>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <span></span>
+                </div>
+            </div>
+            ;
+        </>
+    );
+};
+
+export default GovernRegulationsPage;
---
+++ src/pages/MembershipRegulationsPage/MembershipRegulationsPage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./MembershipRegulationsPage.module.scss";
+
+const MembershipRegulationsPage = () => {
+    return (
+        <>
+            <div className={styles.membershipRegulationsPage}>
+                <div className={styles.membershipRegulationsPageWrapper}>
+                    <span>
+                        УТВЕРЖДЕН Общим собранием членов ОО “Палата Независимых
+                        Директоров Кыргызской Республики” Протокол № Х от «23»
+                        ноября 2024 года
+                    </span>
+                    <h1>УСТАВ</h1>
+                    <h2>
+                        ОБЩЕСТВЕННОГО ОБЪЕДИНЕНИЯ <br /> «ПАЛАТА НЕЗАВИСИМЫХ
+                        ДИРЕКТОРОВ <br /> КЫРГЫЗСКОЙ РЕСПУБЛИКИ»
+                    </h2>
+                    <p>г. БИШКЕК – 2024 год</p>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <span></span>
+                </div>
+            </div>
+            ;
+        </>
+    );
+};
+
+export default MembershipRegulationsPage;
