e00a12c440c97ebb1735d0faa1b34203fe19af1c
SanjarKeySkills <forest2004@mail.ru>
2024-12-09 11:24:42 +0600

    09/12/24 | mainHero scrollBtn completed

--- src/components/Widget/MainBlock/MainHero/mainHero.jsx
+++ src/components/Widget/MainBlock/MainHero/mainHero.jsx
@@ -1,11 +1,59 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./mainHero.module.scss";
 import videoBg from "../../../../assets/Business _ stock video footage by FINDSTORY.mp4";
+import scrollButton from "../../../../assets/arrowScrollDown.png";
 
 const VideoHero = () => {
     const videoRef = useRef(null);
+    const [isAtBottom, setIsAtBottom] = useState(false); // скролл в обе стороны
+
     useEffect(() => {
         videoRef.current.play();
+    }, []);
+
+    // Функция для прокрутки вниз
+    const handleScrollDown = () => {
+        const pageHeight = document.documentElement.scrollHeight;
+        const scrollPosition = window.pageYOffset + window.innerHeight;
+
+        if (scrollPosition < pageHeight - 1) {
+            // Если не внизу страницы, прокручиваем вниз
+            window.scrollTo({
+                top: window.innerHeight + window.pageYOffset,
+                behavior: "smooth",
+            });
+
+            // Функция для прокрутки вниз на 100vh в однц сторону
+            // window.scrollTo({
+            //     top: window.innerHeight + window.pageYOffset, // Прокручиваем на 100vh вниз от текущей позиции
+            //     behavior: "smooth", // Плавная прокрутка
+            // });
+        } else {
+            // Если достигнут низ страницы, прокручиваем вверх
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+    };
+
+    // Проверка, если достигнут конец страницы
+    useEffect(() => {
+        const checkIfAtBottom = () => {
+            const pageHeight = document.documentElement.scrollHeight;
+            const scrollPosition = window.pageYOffset + window.innerHeight;
+            if (scrollPosition >= pageHeight - 1) {
+                setIsAtBottom(true);
+            } else {
+                setIsAtBottom(false);
+            }
+        };
+
+        window.addEventListener("scroll", checkIfAtBottom);
+
+        return () => {
+            window.removeEventListener("scroll", checkIfAtBottom);
+        };
     }, []);
 
     return (
@@ -22,6 +70,9 @@ const VideoHero = () => {
                 </div>
                 <video src={videoBg} loop muted ref={videoRef}></video>
             </div>
+            <button onClick={handleScrollDown} className={styles.scrollButton}>
+                <img src={scrollButton} alt="btnImg" />
+            </button>
         </div>
     );
 };
