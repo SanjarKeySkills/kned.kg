7ba6056c47b6fbdf9ba1485f3fc26d1c5a14cbfc
SanjarKeySkills <forest2004@mail.ru>
2024-12-12 03:41:36 +0600

    12/12/24 | deleted partners slider

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,7 +4,7 @@ import WelcomeBlock from "../../components/Widget/MainBlock/WelcomeBlock/welcome
 import SliderCourseIfcBtn from "../../components/Widget/SliderCourseIfcBtn/SliderCourseIfcBtn.jsx";
 import InfoTableBlock from "../../components/Widget/MainBlock/InfoTableBlock/infoTableBlock.jsx";
 import NewsBlock from "../../components/Widget/MainBlock/NewsBlock/newsBlock.jsx";
-import PartnersSlider from "../../components/Widget/MainBlock/PartnersSlider/PartnersSlider.jsx";
+// import PartnersSlider from "../../components/Widget/MainBlock/PartnersSlider/PartnersSlider.jsx";
 import CriteriaMember from "../../components/Widget/MainBlock/CriteriaMember/criteriaMembers.jsx";
 
 const MainPage = () => {
@@ -15,7 +15,7 @@ const MainPage = () => {
             <SliderCourseIfcBtn />
             <InfoTableBlock />
             <NewsBlock />
-            <PartnersSlider />
+            {/* <PartnersSlider /> */}
             <CriteriaMember />
         </>
     );
