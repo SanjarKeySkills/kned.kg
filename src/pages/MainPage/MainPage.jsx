4317afa30b169ede275acf6f249c4f4cf966b572
SanjarKeySkills <forest2004@mail.ru>
2024-12-11 13:32:33 +0600

    11/12/24 | added some members

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import MainHero from "../../components/Widget/MainBlock/MainHero/mainHero.jsx";
-import WelcomeBlock from "../../components/Widget/MainBlock/WelcomeBlock/welcomeBlock.jsx";
-import SliderCourseIfcBtn from "../../components/Widget/SliderCourseIfcBtn/SliderCourseIfcBtn.jsx";
-import InfoTableBlock from "../../components/Widget/MainBlock/InfoTableBlock/infoTableBlock.jsx";
-import NewsBlock from "../../components/Widget/MainBlock/NewsBlock/newsBlock.jsx";
-import CriteriaMember from "../../components/Widget/MainBlock/CriteriaMember/criteriaMembers.jsx";
-import PartnersSlider from "../../components/Widget/MainBlock/PartnersSlider/partnersSlider.jsx";
+// import MainHero from "../../components/Widget/MainBlock/MainHero/mainHero.jsx";
+// import WelcomeBlock from "../../components/Widget/MainBlock/WelcomeBlock/welcomeBlock.jsx";
+// import SliderCourseIfcBtn from "../../components/Widget/SliderCourseIfcBtn/SliderCourseIfcBtn.jsx";
+// import InfoTableBlock from "../../components/Widget/MainBlock/InfoTableBlock/infoTableBlock.jsx";
+// import NewsBlock from "../../components/Widget/MainBlock/NewsBlock/newsBlock.jsx";
+// import CriteriaMember from "../../components/Widget/MainBlock/CriteriaMember/criteriaMembers.jsx";
+// import PartnersSlider from "../../components/Widget/MainBlock/PartnersSlider/partnersSlider.jsx";
 
 const MainPage = () => {
     return (
         <>
-            <MainHero />
+            {/* <MainHero />
             <WelcomeBlock />
             <SliderCourseIfcBtn />
             <InfoTableBlock />
             <NewsBlock />
             <PartnersSlider />
-            <CriteriaMember />
+            <CriteriaMember /> */}
         </>
     );
 };
