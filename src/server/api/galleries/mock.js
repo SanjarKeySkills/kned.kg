1ac6d66032dca0787f5bb98e41d39c78bf41dddd
SanjarKeySkills <forest2004@mail.ru>
2024-12-26 10:49:55 +0600

    26/12/24| changed directory volonetz

--- src/server/api/galleries/mock.js
+++ src/server/api/galleries/mock.js
@@ -1,84 +1,84 @@
 const mockGalleries = [
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_1.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_1.png"),
         id: 1,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_2.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_2.png"),
         id: 2,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_3.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_3.png"),
         id: 3,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_4.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_4.png"),
         id: 4,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_5.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_5.png"),
         id: 5,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_6.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_6.png"),
         id: 7,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_7.jpg"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_7.jpg"),
         id: 8,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_8.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_8.png"),
         id: 9,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_9.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_9.png"),
         id: 10,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_10.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_10.png"),
         id: 11,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_280924/volynetz_11.png"),
+        path: require("../../../assets/businessSaturdayData/280924/volynetz_11.png"),
         id: 12,
     },
     // ---
     {
-        path: require("../../../assets/businessSaturdayData/bs_270424/ahmetova_1.png"),
+        path: require("../../../assets/businessSaturdayData/270424/ahmetova_1.png"),
         id: 13,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_270424/ahmetova_2.png"),
+        path: require("../../../assets/businessSaturdayData/270424/ahmetova_2.png"),
         id: 14,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_270424/ahmetova_3.png"),
+        path: require("../../../assets/businessSaturdayData/270424/ahmetova_3.png"),
         id: 15,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_270424/ahmetova_4.png"),
+        path: require("../../../assets/businessSaturdayData/270424/ahmetova_4.png"),
         id: 16,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_270424/ahmetova_5.png"),
+        path: require("../../../assets/businessSaturdayData/270424/ahmetova_5.png"),
         id: 17,
     },
     // ---
     {
-        path: require("../../../assets/businessSaturdayData/bs_270124/kurmangziev_1.png"),
+        path: require("../../../assets/businessSaturdayData/270124/kurmangziev_1.png"),
         id: 18,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_270124/kurmangziev_2.png"),
+        path: require("../../../assets/businessSaturdayData/270124/kurmangziev_2.png"),
         id: 19,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_270124/kurmangziev_3.png"),
+        path: require("../../../assets/businessSaturdayData/270124/kurmangziev_3.png"),
         id: 20,
     },
     {
-        path: require("../../../assets/businessSaturdayData/bs_270124/kurmangziev_4.png"),
+        path: require("../../../assets/businessSaturdayData/270124/kurmangziev_4.png"),
         id: 21,
     },
     // ---
