3443dddb66deae59468633081b23976025794d66
SanjarKeySkills <forest2004@mail.ru>
2024-12-27 23:51:24 +0600

    27/12/24| mockJs members

--- src/server/api/members/mock.js
+++ src/server/api/members/mock.js
@@ -7,42 +7,6 @@ const mockGalleries = [
         path: require("../../../assets/businessSaturdayData/280924/volynetz_2.png"),
         id: 2,
     },
-    {
-        path: require("../../../assets/businessSaturdayData/280924/volynetz_3.png"),
-        id: 3,
-    },
-    {
-        path: require("../../../assets/businessSaturdayData/280924/volynetz_4.png"),
-        id: 4,
-    },
-    {
-        path: require("../../../assets/businessSaturdayData/280924/volynetz_5.png"),
-        id: 5,
-    },
-    {
-        path: require("../../../assets/businessSaturdayData/280924/volynetz_6.png"),
-        id: 7,
-    },
-    {
-        path: require("../../../assets/businessSaturdayData/280924/volynetz_7.jpg"),
-        id: 8,
-    },
-    {
-        path: require("../../../assets/businessSaturdayData/280924/volynetz_8.png"),
-        id: 9,
-    },
-    {
-        path: require("../../../assets/businessSaturdayData/280924/volynetz_9.png"),
-        id: 10,
-    },
-    {
-        path: require("../../../assets/businessSaturdayData/280924/volynetz_10.png"),
-        id: 11,
-    },
-    {
-        path: require("../../../assets/businessSaturdayData/280924/volynetz_11.png"),
-        id: 12,
-    },
 ];
 
 export default mockGalleries;
