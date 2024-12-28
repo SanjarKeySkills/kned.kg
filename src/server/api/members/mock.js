68b9d0de56d543dd4fefdfe3cf08ba346da07287
SanjarKeySkills <forest2004@mail.ru>
2024-12-28 15:31:59 +0600

    28/12/24| pushed to mockJs activemem 13 mem

--- src/server/api/members/mock.js
+++ src/server/api/members/mock.js
@@ -11,6 +11,46 @@ const mockMembers = [
         path: require("../../../assets/active_members/saltanat_tazhibaeva.png"),
         id: 3,
     },
+    {
+        path: require("../../../assets/active_members/aaly_umakulov.png"),
+        id: 4,
+    },
+    {
+        path: require("../../../assets/active_members/nurdin_sultanbaev.png"),
+        id: 5,
+    },
+    {
+        path: require("../../../assets/active_members/zhanna_toktosunova.png"),
+        id: 6,
+    },
+    {
+        path: require("../../../assets/active_members/kubanych_abdraimov.png"),
+        id: 7,
+    },
+    {
+        path: require("../../../assets/active_members/nurgul_torobaeva.png"),
+        id: 8,
+    },
+    {
+        path: require("../../../assets/active_members/sultan_aiylchiev.png"),
+        id: 9,
+    },
+    {
+        path: require("../../../assets/active_members/gulsaara_atanova.png"),
+        id: 10,
+    },
+    {
+        path: require("../../../assets/active_members/ulan_bazarkulov.png"),
+        id: 11,
+    },
+    {
+        path: require("../../../assets/active_members/daniyar_amatov.png"),
+        id: 12,
+    },
+    {
+        path: require("../../../assets/active_members/sanjar_abakirov.png"),
+        id: 13,
+    },
 ];
 
 export default mockMembers;
