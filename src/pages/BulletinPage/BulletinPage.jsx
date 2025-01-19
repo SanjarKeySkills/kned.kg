0411ebba47e8ecf5b46c6de7678ca7c3c925d632
SanjarKeySkills <forest2004@mail.ru>
2025-01-19 22:29:45 +0600

    19/01/25| seccess with the slider Bulletine Page

--- src/pages/BulletinPage/BulletinPage.jsx
+++ src/pages/BulletinPage/BulletinPage.jsx
@@ -1,62 +1,80 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, {
+    useState,
+    useEffect,
+    //  useRef
+} from "react";
 import styles from "./bulletinPage.module.scss";
 import { useParams } from "react-router-dom";
 import HeroHeader from "../../components/Widget/LayoutsComponentsBlock/HeroHeader/heroHeader.jsx";
 // import FormatText from "../../components/Widget/FormatText/FormatText.jsx";
 import newsData from "../../components/Widget/NewsBlock/NewsCards/newsData.js";
+// import { getGalleryList } from "../../server/api/galleries/id";
+import { getGalleryPathById } from "../../server/api/galleries/id";
 
 const BulletinPage = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
 
-    const { id } = useParams();
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const touchStartX = useRef(0);
-    const touchEndX = useRef(0);
+    const { id } = useParams(); // Receive id from URL parameters
+    const [currentIndex, setCurrentIndex] = useState(0); // the index of current img
+    const [galleryImages, setGalleryImages] = useState(0); // the array of img for slider
+    // const touchStartX = useRef(0);
+    // const touchEndX = useRef(0);
 
-    const news = newsData.find((item) => item.id === id);
-    if (!news || !news.images || news.images.length === 0) {
-        return <p>Альбом не найден</p>;
-    }
+    // hiding the obj of the news by id
+    const newsItem = newsData.find((item) => item.id === id);
+    // if (!news || !news.images || news.images.length === 0) {
+    //     return <p>Альбом не найден</p>;
+    // }
 
-    const images = news.images;
+    // if news was founded - initializing slider with img
+    useEffect(() => {
+        if (newsItem && newsItem.images) {
+            setGalleryImages(newsItem.images);
+        }
+    }, [newsItem]);
 
-    // для перехода к следующему изображению
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    // slider functions
+    const nextImage = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % galleryImages.length);
+    };
+
+    const prevImage = () => {
+        setCurrentIndex(
+            (prevIndex) =>
+                (prevIndex - 1 + galleryImages.length) % galleryImages.length
         );
     };
 
     // для перехода к предыдущему изображению
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === 0 ? images.length - 1 : prevIndex - 1
-        );
-    };
+    // const prevSlide = () => {
+    //     setCurrentIndex((prevIndex) =>
+    //         prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    //     );
+    // };
 
-    const handleTouchStart = (e) => {
-        touchStartX.current = e.touches[0].clientX;
-    };
-    const handleTouchMove = (e) => {
-        touchEndX.current = e.touches[0].clientX;
-    };
+    // const handleTouchStart = (e) => {
+    //     touchStartX.current = e.touches[0].clientX;
+    // };
+    // const handleTouchMove = (e) => {
+    //     touchEndX.current = e.touches[0].clientX;
+    // };
 
-    const handleTouchEnd = () => {
-        if (touchStartX.current - touchEndX.current > 50) {
-            nextSlide();
-        } else if (touchStartX.current - touchEndX.current < -50) {
-            prevSlide();
-        }
-    };
+    // const handleTouchEnd = () => {
+    //     if (touchStartX.current - touchEndX.current > 50) {
+    //         nextSlide();
+    //     } else if (touchStartX.current - touchEndX.current < -50) {
+    //         prevSlide();
+    //     }
+    // };
 
     return (
         <>
             <HeroHeader />
             <div className={styles.newsPage}>
                 <div className={styles.newsPageContainer}>
-                    <div
+                    {/* <div
                         onTouchStart={handleTouchStart}
                         onTouchMove={handleTouchMove}
                         onTouchEnd={handleTouchEnd}>
@@ -89,6 +107,25 @@ const BulletinPage = () => {
                                 ❯
                             </button>
                         )}
+                    </div> */}
+
+                    <div className="slider">
+                        {galleryImages.length > 0 && (
+                            <div className="slider-container">
+                                <img
+                                    src={getGalleryPathById(
+                                        galleryImages[currentIndex].id
+                                    )}
+                                    alt="gallery"
+                                    className="slider-image"
+                                />
+                            </div>
+                        )}
+
+                        <div className="slider-controls">
+                            <button onClick={prevImage}>{"<"}</button>
+                            <button onClick={nextImage}>{">"}</button>
+                        </div>
                     </div>
 
                     {/* <div className={styles.titleNewsWrapper}>
