f55931bac341dd563a8e8595d59d064a028bcf28
SanjarKeySkills <forest2004@mail.ru>
2025-01-20 22:06:51 +0600

    20/01/25| itteration popUp none header

--- src/components/Widget/LayoutsComponentsBlock/Header/header.jsx
+++ src/components/Widget/LayoutsComponentsBlock/Header/header.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./header.module.scss";
 import { Link } from "react-router-dom";
 import logo from "../../../../assets/imageMainHeader.png";
 import burgerButton from "../../../../assets/burgerButton.svg";
 import arrowPopUp from "../../../../assets/arrowPopUp.svg";
-import { useState } from "react";
 
 const Header = () => {
     const [isOpenPopUp, setOpenPopUp] = useState("none");
@@ -13,18 +12,25 @@ const Header = () => {
     const togglePopUp = () => {
         setOpenPopUp((prev) => (prev === "none" ? "block" : "none"));
     };
+
     const togglePndMenu = () => {
         setPndMenuOpen((prev) => !prev);
     };
-    const changeStyle = () => {
-        if (isOpenPopUp === "none") {
-            setOpenPopUp("block");
-        } else setOpenPopUp("none");
-    };
+
+    // const changeStyle = () => {
+    //     if (isOpenPopUp === "none") {
+    //         setOpenPopUp("block");
+    //     } else setOpenPopUp("none");
+    // };
 
     // Функция для закрытия меню при клике на ссылку
     const handleLinkClick = () => {
-        setPndMenuOpen(false);
+        setPndMenuOpen(false); // Закрывает выпадающее меню "О ПАЛАТЕ"
+        setOpenPopUp("none"); // Закрывает адаптивное меню popUp
+    };
+    // Функция для закрытия адаптивного popUp меню при клике на ссылку
+    const handlePopUpLinkClick = () => {
+        setOpenPopUp("none"); // Закрытие адаптивного popUp меню
     };
 
     return (
@@ -131,11 +137,13 @@ const Header = () => {
                     </div>
                 </div>
             </div>
+
             <div className={styles.headerMobile}>
                 <Link to="/">
                     <img src={logo} alt="main" />
                 </Link>
-                <img src={burgerButton} alt="burger" onClick={changeStyle} />
+                {/* <img src={burgerButton} alt="burger" onClick={changeStyle} /> */}
+                <img src={burgerButton} alt="burger" onClick={togglePopUp} />
                 <div
                     className={styles.containerPopUp}
                     style={{ display: isOpenPopUp }}>
@@ -144,28 +152,47 @@ const Header = () => {
                             <img
                                 src={arrowPopUp}
                                 alt="arrow"
-                                onClick={changeStyle}
+                                onClick={togglePopUp}
                             />
                         </div>
                         <ul className={styles.listTopPopUp}>
                             <li>
-                                <Link to="/">НА ГЛАВНУЮ</Link>
+                                <Link
+                                    to="/"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.link}>
+                                    НА ГЛАВНУЮ
+                                </Link>
                             </li>
                             <li>
-                                <Link to="/courses">КУРСЫ И ТРЕНИНГИ</Link>
+                                <Link
+                                    to="/courses"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.link}>
+                                    КУРСЫ И ТРЕНИНГИ
+                                </Link>
                             </li>
                             <li>
-                                <Link to="/news" className={styles.link}>
+                                <Link
+                                    to="/news"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.link}>
                                     НОВОСТИ
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/articles" className={styles.link}>
+                                <Link
+                                    to="/articles"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.link}>
                                     ПУБЛИКАЦИИ
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/gallery" className={styles.link}>
+                                <Link
+                                    to="/gallery"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.link}>
                                     ГАЛЕРЕЯ
                                 </Link>
                             </li>
@@ -175,34 +202,52 @@ const Header = () => {
                                 </a>
                             </li> */}
                         </ul>
-                        <Link className={styles.buttonHero} to="/contact">
+                        <Link
+                            onClick={handlePopUpLinkClick}
+                            className={styles.buttonHero}
+                            to="/contact">
                             ВСТУПИТЬ В ПАЛАТУ
                         </Link>
                     </div>
                     <div className={styles.headerBottomPopUp}>
                         <ul className={styles.listBottomPopUp}>
                             <li>
-                                <Link to="/members" className={styles.link}>
+                                <Link
+                                    to="/members"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.link}>
                                     НАШИ ЭКСПЕРТЫ
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/about" className={styles.link}>
+                                <Link
+                                    to="/about"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.link}>
                                     О ПАЛАТЕ
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/board" className={styles.linkTop}>
+                                <Link
+                                    to="/board"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.linkTop}>
                                     ОРГАНЫ УПРАВЛЕНИЯ
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/services" className={styles.link}>
+                                <Link
+                                    to="/services"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.link}>
                                     УСЛУГИ
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/contact" className={styles.link}>
+                                <Link
+                                    to="/contact"
+                                    onClick={handlePopUpLinkClick}
+                                    className={styles.link}>
                                     КОНТАКТЫ
                                 </Link>
                             </li>
