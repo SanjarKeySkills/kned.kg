e02506e95dbdfe884e71e8fdae95d6bf55e4158b
SanjarKeySkills <forest2004@mail.ru>
2025-01-22 21:04:36 +0600

    22/01/25| corpdirPaghe Hero completed 80%

--- src/components/Widget/CorpdirBlock/CorpdirBlock.jsx
+++ src/components/Widget/CorpdirBlock/CorpdirBlock.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import styles from "./corpdirBlock.module.scss";
+import { Link } from "react-router-dom";
 
 export default function Corpdir() {
     return (
         <div className={styles.corpdirBlock}>
             <div className={styles.heroCorpdirBlock}>
-                <p>Test</p>
+                <h1>Корпоративный директор</h1>
+                <p>
+                    Профессиональный сертификационный образовательный курс
+                    <br />
+                    Начало обучения - 28 февраля 2025 г.
+                </p>
+                <div className={styles.heroCorpdirBlockBtnWrapper}>
+                    <Link
+                        className={styles.headerBtnApply}
+                        to="https://forms.gle/SpiszP9pbvSdBGQF8">
+                        ПОДАТЬ ЗАЯВКУ НА ОБУЧЕНИЕ
+                    </Link>
+                    <Link onClick={"#"} className={styles.headerBtnInfo}>
+                        Узнать подробнее
+                    </Link>
+                </div>
             </div>
         </div>
     );
