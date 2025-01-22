5dd597aa85fd74124248c00667b5dfca313988a3
SanjarKeySkills <forest2004@mail.ru>
2025-01-22 16:47:14 +0600

    22/01/25| itteration for Suspence and chunks

--- src/pages/MainPage/MainPage.async.jsx
+++ src/pages/MainPage/MainPage.async.jsx
@@ -1,5 +1,5 @@
 import { lazy } from "react";
 
-const MainPageAsync = lazy(() => import("./MainPage"));
+const MainPageAsync = lazy(() => import("./MainPage.jsx"));
 
 export { MainPageAsync as MainPage };
