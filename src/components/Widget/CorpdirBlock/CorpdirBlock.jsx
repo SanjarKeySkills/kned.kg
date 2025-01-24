c75a02e80c3141db8e13d61b801489bd6e3031d2
SanjarKeySkills <forest2004@mail.ru>
2025-01-24 19:14:20 +0600

    24/01/25| itterations with courses block CorpDir

--- src/components/Widget/CorpdirBlock/CorpdirBlock.jsx
+++ src/components/Widget/CorpdirBlock/CorpdirBlock.jsx
@@ -5,43 +5,47 @@ import studyScale from "../../../assets/courses/corpdir_hero_studySkale.png";
 
 export default function Corpdir() {
     return (
-        <div className={styles.corpdirBlock}>
-            <div className={styles.heroCorpdirBlock}>
+        <div className={styles.course}>
+            <div className={styles.heroCourse}>
                 <h1>Корпоративный директор</h1>
                 <p>
                     Профессиональный сертификационный образовательный курс
                     <br />
                     Начало обучения - 28 февраля 2025 г.
                 </p>
-                <div className={styles.heroCorpdirBlockBtnWrapper}>
+                <div className={styles.heroBtnWrapper}>
                     <Link
-                        className={styles.headerBtnApply}
+                        className={styles.heroBtnApply}
                         to="https://forms.gle/SpiszP9pbvSdBGQF8">
                         ПОДАТЬ ЗАЯВКУ НА ОБУЧЕНИЕ
                     </Link>
-                    <Link onClick={"#"} className={styles.headerBtnInfo}>
+                    <Link onClick={"/"} className={styles.heroBtnInfo}>
                         Узнать подробнее
                     </Link>
                 </div>
             </div>
-            <div className={styles.corpdirStudy}>
-                <h1 className={styles.corpdirStudyTitle}>
+            <div className={styles.courseStudy}>
+                <h1 className={styles.courseTitle}>
                     Обучение на курсе "Корпоративный директор", это:
                 </h1>
-                <div className={styles.studyListItemWrapper}>
-                    <div className={styles.studyListItemLeft}>
-                        <p className={styles.studyListContent}>
+                {/* list */}
+                <ul className={styles.studyList}>
+                    <li className={styles.listItem}>
+                        <div className={styles.numbers}>1</div>
+                        <p className={styles.itemContent}>
                             <b>Передовые знания</b> о роли и функциях совета
                             директоров / директоров в современной компании.
                         </p>
-                    </div>
-                    <div className={styles.studyListItemRight}>
-                        <p className={styles.studyListContent}>
+                    </li>
+                    <li className={styles.listItem}>
+                        <div className={styles.numbers}>2</div>
+                        <p className={styles.itemContent}>
                             <b>Развитие карьерного потенциала.</b>
                         </p>
-                    </div>
-                    <div className={styles.studyListItemLeft}>
-                        <p className={styles.studyListContent}>
+                    </li>
+                    <li className={styles.listItem}>
+                        <div className={styles.numbers}>3</div>
+                        <p className={styles.itemContent}>
                             <b>Доступ в эксклюзивное сообщество выпускников</b>
                             программ по корпоративному управлению Палаты
                             независимых директоров Кыргызской Республики, что
@@ -49,9 +53,10 @@ export default function Corpdir() {
                             взаимодействия, обмена опытом и профессионального
                             развития.
                         </p>
-                    </div>
-                    <div className={styles.studyListItemRight}>
-                        <p className={styles.studyListContent}>
+                    </li>
+                    <li className={styles.listItem}>
+                        <div className={styles.numbers}>4</div>
+                        <p className={styles.itemContent}>
                             <b>Сертификат</b> Палаты независимых директоров
                             Кыргызской Республики, подтверждающий квалификацию в
                             качестве сертифицированного корпоративного
@@ -59,13 +64,53 @@ export default function Corpdir() {
                             уровня и готовности к выполнению соответствующих
                             функций.
                         </p>
-                    </div>
-                    <img
+                    </li>
+                </ul>
+                {/* ------------------------------------------------------- */}
+                {/* <div className={styles.listItemsWrapper}>
+                        <div className={styles.studyListItemLeft}>
+                            <div className={styles.numbers}>1</div>
+                            <p className={styles.itemContent}>
+                                <b>Передовые знания</b> о роли и функциях совета
+                                директоров / директоров в современной компании.
+                            </p>
+                        </div>
+                        <div className={styles.studyListItemRight}>
+                            <div className={styles.numbers}>2</div>
+                            <p className={styles.studyListContent}>
+                                <b>Развитие карьерного потенциала.</b>
+                            </p>
+                        </div>
+                        <div className={styles.studyListItemLeft}>
+                            <div className={styles.numbers}>3</div>
+                            <p className={styles.itemContent}>
+                                <b>
+                                    Доступ в эксклюзивное сообщество выпускников
+                                </b>
+                                программ по корпоративному управлению Палаты
+                                независимых директоров Кыргызской Республики,
+                                что предоставляет возможности для сетевого
+                                взаимодействия, обмена опытом и
+                                профессионального развития.
+                            </p>
+                        </div>
+                        <div className={styles.studyListItemRight}>
+                            <div className={styles.numbers}>4</div>
+                            <p className={styles.itemContent}>
+                                <b>Сертификат</b> Палаты независимых директоров
+                                Кыргызской Республики, подтверждающий
+                                квалификацию в качестве сертифицированного
+                                корпоративного директора, что является
+                                признанием профессионального уровня и готовности
+                                к выполнению соответствующих функций.
+                            </p>
+                        </div>
+                    </div> */}
+                {/* <img
                         src={studyScale}
                         alt="scale"
-                        className={styles.listScaleBackground}
-                    />
-                </div>
+                        className={styles.listScaleBackground1}
+                    /> */}
             </div>
         </div>
     );
