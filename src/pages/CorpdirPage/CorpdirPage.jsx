5bff148854b4bb66123356fa48e3ecfacd99a822
SanjarKeySkills <forest2004@mail.ru>
2025-01-30 12:18:44 +0600

    30/01/25| Cholpon_Eshberdieva added new

--- src/pages/CorpdirPage/CorpdirPage.jsx
+++ src/pages/CorpdirPage/CorpdirPage.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import HeroCourse from "../../components/Widget/CorpdirBlock/HeroCourse/HeroCourse.jsx";
-
-// import Accordion from "../../components/Widget/CorpdirBlock/Accordion/Accordion.jsx";
-// import CorpdirAbout from "../../components/Widget/CorpdirBlock/CorpdirAbout/CorpdirAbout.jsx";
+import Accordion from "../../components/Widget/CorpdirBlock/Accordion/Accordion.jsx";
+import CorpdirAbout from "../../components/Widget/CorpdirBlock/CorpdirAbout/CorpdirAbout.jsx";
 import Participants from "../../components/Widget/CorpdirBlock/Participants/Participants.jsx";
 import CorpdirInfo from "../../components/Widget/CorpdirBlock/CorpdirInfo/corpdirInfo.jsx";
 import CorpdirAnnotation from "../../components/Widget/CorpdirBlock/CorpdirAnnotation/CorpdirAnnotation.jsx";
@@ -18,8 +17,8 @@ const CorpdirPage = () => {
             <CorpdirInfo />
             <CorpdirAnnotation />
             <Participants />
-            {/* <CorpdirAbout /> */}
-            {/* <Accordion /> */}
+            <CorpdirAbout />
+            <Accordion />
         </div>
     );
 };
